0f67247ded15350a994f7e845f991a15db6ea9a4
fluxequalsrad <155498995+fluxequalsrad@users.noreply.github.com>
2024-01-05 12:30:18 +1100

    Update main.py
    Added code to extract and save product names as well as check for the existence of the 'sold out' tag. This information is then added to a data frame for ease of access.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import requests
 import random
 from bs4 import BeautifulSoup
+import pandas as pd
+
 
 # list of user agents to resolve 403 forbidden error
 userAgents = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.1',
@@ -14,18 +16,28 @@ userAgents = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1
 website = "https://hamishandandy.com/shop/"
 html_data = requests.get(website, headers={'User-Agent': random.choice(userAgents)})
 
-#print(html_data.text)
-
 # Parse HTML Code
 # create beautiful soup object
 soup = BeautifulSoup(html_data.content, "html.parser")
 
 shop_elements = soup.find_all("div", class_="c-product-tile__wrap")
-print(shop_elements)
+#print(shop_elements)
 
+# create lists to store product names and 'in stock' information
+product_list = []
+stock_list = []
+
+# iterate through the shop elements extracting product names and checking if the soldout tag exists
+# append this information to the relevant lists
 for element in shop_elements:
     product_element = element.find_all("h5")
     stock_element = element.find_all("span", class_="soldout-tag")
-    print(product_element, end="\n"*2)
-    print(stock_element, end="\n"*2)
-    
+    product_text = product_element[0].text
+    product_list.append(product_text)
+    stock_text = stock_element[0].text if stock_element else "Available"
+    stock_list.append(stock_text)
+
+# create dataframe using list information
+product_data = pd.DataFrame({"Product": product_list, "In Stock": stock_list})
+
+print(product_data)
